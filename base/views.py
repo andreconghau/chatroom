79dade3a0a3412dc26a3640ce4dd65e56920dc5e
hau <hau>
2024-09-09 14:50:27 +0700

    Refactor views and templates for displaying rooms on the home page

--- base/views.py
+++ base/views.py
@@ -3,8 +3,15 @@ from django.http import HttpResponse
 
 # Create your views here.
 
+rooms = [
+    {'id': 1, "name": "Room 1", "status": "Available"},
+    {'id': 2, "name": "Room 2", "status": "Available"},
+    {'id': 3, "name": "Room 3", "status": "Unavailable"},
+]
+
 def home(request):
-    return render(request, "pages/index.html")
+    context = {'rooms': rooms}
+    return render(request, "pages/index.html", context)
 
 def room(request):  
     return render(request, "room.html")
