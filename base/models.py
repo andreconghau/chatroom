2a7f7f8eea5b892da7796450cd3f230df57fc866
hau <hau>
2024-09-10 09:49:49 +0700

    Refactor models.py to add Room model

--- base/models.py
+++ base/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Room(models.Model):
+    # hosts
+    # topic
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    # participants
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.name
